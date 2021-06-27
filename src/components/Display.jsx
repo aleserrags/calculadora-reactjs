46d6801a050ba62bd7d10b67c892d2c74570beab
Alexandre Serra <alexandre.serra.dev@gmail.com>
2021-06-27 19:33:10 -0400

    Calculadora finalizada

---
+++ src/components/Display.jsx
@@ -0,0 +1,10 @@
+import './Display.css'
+import React from 'react'
+
+export default function Display(props) {
+    return (
+        <div className="display">
+            {props.value}
+        </div>
+    )
+}
